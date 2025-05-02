4d160f060e64d58d8630617e484e9759ab4af115
Aizen-Uchiha <alphadaryl0425@gmail.com>
2025-05-02 10:04:41 +0800

    "FIXED TITLE"

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'Calumpang Rural Health Unit',
     'providers' => [
         /*
          * Laravel Framework Service Providers...
